8924e4d734c2bfd541cead184bc528777fc9d77d
Danny0 <107776928+Danny-LLi@users.noreply.github.com>
2026-03-24 11:31:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/DannySec/" : "/",
+  base: mode === "production" ? (process.env.VITE_BASE_PATH ?? "/DannySec/") : "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
